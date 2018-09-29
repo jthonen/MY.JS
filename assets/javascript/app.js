c990e619678d919e482084c4fabbcc85edf3d666
unknown <ming29398@gmail.com>
2018-09-29 13:41:03 -0700

    added input validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,49 @@
 $(document).ready(function () {
     console.log("ready!");
 
-
     $("#searchTrend").on("click", function (event) {
         event.preventDefault();
         console.log("searchTrend click");
         var trendVal = $("#trendTextId").val();
-        console.log("trendVal : "+trendVal);
+        console.log("trendVal : " + trendVal);
+        var trend = validTrendInput(trendVal);
     })
+
+    // eliminate empty stock name
+    function validStockInput(stockVal) {
+        var stocks = [];
+        stocks = stockVal.split(",");
+        for (var i = stocks.length - 1; i >= 0; i--) {
+            stocks[i] = stocks[i].trim();
+            if (stocks[i] === "") {
+                stocks.splice(i, 1);
+            }
+        }
+        return stocks;
+    }
+
+    function validTrendInput(trend) {
+        if (trend === "") {
+            console.log("trend is empty");
+        }
+        else {
+            console.log("good");
+        }
+        return trend;
+    }
 
     $("#searchStock").on("click", function (event) {
         event.preventDefault();
-        console.log("searchstock click");
         var stockVal = $("#stockTextId").val();
-        console.log("stockVal : "+stockVal);
+        var stocks = [];
+        stocks = validStockInput(stockVal);
+        // DEBUG print out the array values
+        for (var i = 0; i < stocks.length; i++) {
+            console.log("name: " + stocks[i]);
+        }
+        $("#google-trend-img").empty();
+        var image = $("<img>").attr("src", "assets/images/trends-example.png");
+        $("#google-trend-img").append(image);
+
     })
 })
