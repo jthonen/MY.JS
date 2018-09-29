f13be54836f29d73f2bc67f2aeb20a066777e84f
Stacey Carrillo <staceycarrillo@Staceys-MacBook-Pro.local>
2018-09-29 14:01:36 -0700

    background image added to navbar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
     console.log("ready!");
 
+    #navigation,.navbar .navbar-default{
+        background-image: url("assets/images/flower.jpg");
+      }
+
 
     $("#searchTrend").on("click", function (event) {
         event.preventDefault();
         console.log("searchTrend click");
-    })
+})
 
     $("#searchStock").on("click", function (event) {
         event.preventDefault();
