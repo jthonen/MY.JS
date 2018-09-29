7881877158a0142e37759da9d84a213abb3b6618
unknown <ming29398@gmail.com>
2018-09-29 11:14:06 -0700

    improved ui

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,14 @@ $(document).ready(function () {
     $("#searchTrend").on("click", function (event) {
         event.preventDefault();
         console.log("searchTrend click");
+        var trendVal = $("#trendTextId").val();
+        console.log("trendVal : "+trendVal);
     })
 
     $("#searchStock").on("click", function (event) {
         event.preventDefault();
         console.log("searchstock click");
+        var stockVal = $("#stockTextId").val();
+        console.log("stockVal : "+stockVal);
     })
 })
