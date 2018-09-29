cb5bdf537ffb8b910aed08bf1719da2256a3f701
unknown <ming29398@gmail.com>
2018-09-29 09:23:53 -0700

    improved search box

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
     console.log("ready!");
-});
+
+
+    $("#searchTrend").on("click", function (event) {
+        event.preventDefault();
+        console.log("searchTrend click");
+    })
+
+    $("#searchStock").on("click", function (event) {
+        event.preventDefault();
+        console.log("searchstock click");
+    })
+})
