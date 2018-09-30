9600463a96d086fb174035d7dea925744264336b
unknown <ming29398@gmail.com>
2018-09-30 08:10:32 -0700

    more validate input fields and cleanup index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,84 @@
+
+var stockTerm = [];
+var trendTerm = null;
+var isTrendValid = false;
+var isStockValid = false;
+
 $(document).ready(function () {
-    console.log("ready!");
+
+    initGraph();     // display example stock graph 
 
     $("#searchTrend").on("click", function (event) {
         event.preventDefault();
         console.log("searchTrend click");
         var trendVal = $("#trendTextId").val();
-        console.log("trendVal : " + trendVal);
-        var trend = validTrendInput(trendVal);
+        isTrendValid = validateTrendInput(trendVal);
+        if (isTrendValid && isStockValid) { 
+            displayGraph();
+        }
     })
 
     // eliminate empty stock name
-    function validStockInput(stockVal) {
+    function validateStockInput(stockVal) {
         var stocks = [];
-        stocks = stockVal.split(",");
-        for (var i = stocks.length - 1; i >= 0; i--) {
-            stocks[i] = stocks[i].trim();
-            if (stocks[i] === "") {
-                stocks.splice(i, 1);
-            }
-        }
-        return stocks;
-    }
-
-    function validTrendInput(trend) {
-        if (trend === "") {
-            console.log("trend is empty");
+        if (stockVal === "") {
+            console.log("stock is empty");
+            $('#empty-stock').text(" *Stock symbol can not be empty");
+            setTimeout(function () {
+                $('#empty-stock').text("");
+            }, 2000);
+            isStockValid = false;
         }
         else {
-            console.log("good");
+            stocks = stockVal.split(",");
+            for (var i = stocks.length - 1; i >= 0; i--) {
+                stocks[i] = stocks[i].trim();
+                if (stocks[i] === "") {
+                    stocks.splice(i, 1);
+                }
+            }
+            isStockValid = true;
         }
-        return trend;
+        stockTerm = stocks;
+        return isStockValid;
+    }
+
+    function validateTrendInput(trend) {
+        if (trend === "") {
+            console.log("trend is empty");
+            $('#empty-search').text(" *Trend can not be empty");
+            setTimeout(function () {
+                $('#empty-search').text("");
+            }, 2000);
+            isTrendValid = false;
+        }
+        else {
+            isTrendValid = true;
+        }
+        trendTerm = trend;
+        return isTrendValid;
+    }
+
+    function initGraph() {
+        $("#google-trend-img").empty();
+        var image = $("<img>").attr("src", "assets/images/trends-example.png");
+        $("#google-trend-img").append(image);
+    }
+
+    // calling api here and display the graph
+    function displayGraph() {
+        $("#google-trend-img").empty();
+        var image = $("<img>").attr("src", "assets/images/trends-example.png");
+        $("#google-trend-img").append(image);
     }
 
     $("#searchStock").on("click", function (event) {
         event.preventDefault();
         var stockVal = $("#stockTextId").val();
-        var stocks = [];
-        stocks = validStockInput(stockVal);
-        // DEBUG print out the array values
-        for (var i = 0; i < stocks.length; i++) {
-            console.log("name: " + stocks[i]);
-        }
-        $("#google-trend-img").empty();
-        var image = $("<img>").attr("src", "assets/images/trends-example.png");
-        $("#google-trend-img").append(image);
+        isStockValid = validateStockInput(stockVal);
 
+        if (isTrendValid && isStockValid) {
+            displayGraph();
+        }
     })
 })
