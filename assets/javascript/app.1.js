6bb889b3473ba5f2acf5a3dc0b82230a212e99fa
unknown <ming29398@gmail.com>
2018-10-02 18:32:10 -0700

    added business news and portfolio pages

--- assets/javascript/app.1.js
+++ assets/javascript/app.1.js
@@ -8,8 +8,6 @@ $(document).ready(function () {
 
     initGraph();     // display example stock graph 
 
-    getBusinessNews();
-
     $("#searchTrend").on("click", function (event) {
         event.preventDefault();
         console.log("searchTrend click");
@@ -83,36 +81,4 @@ $(document).ready(function () {
             displayGraph();
         }
     })
-
-    function getBusinessNews() {
-        // call ajax to get latest business
-        $("#article-id").empty();
-        var name = $(this).attr("data-name");
-        //var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=4JIvycUq3oYpZEEoUjlZhfz5orHCbC9D&q=" + name + "&limit=10&offset=0&rating=R&lang=en";
-        var queryURL = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e20759f693844c8d9336061b37eb2d02";
-        // Creates AJAX call 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-                $("#article-id").empty();
-                var results = response.articles;
-                for (i = 0; i < results.length; i++) {
-                    var listDiv = $("<div>");
-                    var source = $("<p2>").addClass("font-italic").text("source: " + results[i].source.name);
-                    source.append("<br>");
-                    var newsHeading = $("<a>");
-                    newsHeading.addClass("text-primary");
-                    newsHeading.addClass("font-weight-bold");
-                    newsHeading.attr("href", results[i].url);
-                    newsHeading.attr("target", "_blank");
-                    newsHeading.append(results[i].title);
-                    var description = $("<h5>");
-                    description.append(results[i].description);
-                    listDiv.append(source).append(newsHeading).append(description);
-                    //add new div to existing divs
-                    $("#article-id").append(listDiv);
-                }        
-        });
-    }
 })
